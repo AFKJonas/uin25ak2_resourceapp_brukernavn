16538cb40c30268b8a0806b5f6269a56836d9e09
AFKJonas <jonasengelsgaard@gmail.com>
2025-03-03 23:40:30 +0100

    Påbegynt arbeid
    Satt opp litt filer/base

---
+++ uin25ak2_resourceapp_brukernavn/src/components/Layout.jsx
@@ -0,0 +1,16 @@
+export default function Layout() {
+    return (
+        <div>
+            <header>
+                <h1>Min app</h1>
+            </header>
+            <main>
+            </main>
+            <footer>
+                <p>&copy; 2021</p>
+            </footer>
+        </div>
+    );
+}
+
+
